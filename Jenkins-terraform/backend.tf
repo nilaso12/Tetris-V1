5fec31d8713d06e0bff8cce47dadd663374d7162
Dakiswiligri Ouedraogo <106689385+nilaso12@users.noreply.github.com>
2024-04-04 11:15:52 -0500

    Update backend.tf

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dakis-cloud" # Replace with your actual S3 bucket name
+    bucket = "dakis-s3" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "us-east-1"
   }
